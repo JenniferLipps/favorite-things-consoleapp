1980997ed6a555229593c9e520d375616f97e84d
Jennifer Lipps <jjlipps@gmail.com>
2019-08-31 10:58:59 -0500

    setup

---
+++ FavoriteThings/FavThings/Animals.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FavoriteThings.FavThings
+{
+    enum AnimalColor
+    {
+        Brown,
+        Black,
+        Red,
+        Green,
+        White,
+        Spots,
+        Stripes
+    }
+
+    enum AnimalSpecies
+    {
+        Dog,
+        Cat,
+        Horse,
+        Snake,
+        Bird,
+        Lizard
+    }
+
+    class Animal
+    {
+        public AnimalSpecies Species { get; set; }
+
+        public AnimalColor Color { get; set; }
+
+        public string Name { get; set; }
+
+
+        public Animal(string name, AnimalSpecies species, AnimalColor color)
+        {
+            Name = name;
+            Species = species;
+            Color = color;
+
+        }
+        
+
+    }
+    
+}
